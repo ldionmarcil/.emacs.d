dc185f06b334d6e95698c69a9adaefa181903b8a
Louis Dion-Marcil <maden.ldm@gmail.com>
2013-12-19 13:19:13 -0500

    Added ELPA, marmalade and melpa package archives

--- init.el
+++ init.el
@@ -200,6 +200,12 @@
 
 ;;}}}
 
+;;{{{package archives
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+;;}}}
+
 ;;{{{experimental code and misc. funcs/configs 
 
 (defun my-days-to-date (date)
