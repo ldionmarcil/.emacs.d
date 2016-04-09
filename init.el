96a0f6069d62d1b679afba6527256d3d7aa25dde
Louis Dion-Marcil <louis.dionmarcil@gmail.com>
2016-04-08 22:40:11 -0400

    Render JSP files through web-mode

--- init.el
+++ init.el
@@ -135,6 +135,7 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/web-mode")
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 
