4217561723b66c12014cfd50bb4c38cf463f0ca3
Louis Dion-Marcil <ldmarcil@gosecure.ca>
2018-05-07 15:46:44 -0400

    Added buffered-keyword highlighting

--- init.el
+++ init.el
@@ -407,6 +407,31 @@
 
 ;;}}}
 
+;;{{{highlights
+
+(defun find-overlays-specifying (prop pos) 
+  (let ((overlays (overlays-at pos)) 
+	found) 
+    (while overlays 
+      (let ((overlay (car overlays))) 
+	(if (overlay-get overlay prop) 
+	    (setq found (cons overlay found)))) 
+      (setq overlays (cdr overlays))) 
+    found))
+(defun highlight-or-dehighlight-line () 
+  (interactive) 
+  (if (find-overlays-specifying 
+       'line-highlight-overlay-marker 
+       (line-beginning-position)) 
+      (remove-overlays (line-beginning-position) (+ 1 (line-end-position))) 
+    (let ((overlay-highlight (make-overlay 
+			      (line-beginning-position) 
+			      (+ 1 (line-end-position))))) 
+      (overlay-put overlay-highlight 'face '(:background "DarkOrange4")) 
+      (overlay-put overlay-highlight 'line-highlight-overlay-marker t))))
+(global-set-key [f8] 'highlight-or-dehighlight-line)
+
+;;}}}
 
 
 ;; Added by Package.el.  This must come before configurations of
