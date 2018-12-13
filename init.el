4473c73a86b72abf0f3971d14949fd006b181a7f
Louis Dion-Marcil <louis.dionmarcil@10-r0fhhtdh-3v3.fireeye.com>
2018-12-12 19:41:13 -0500

    Add erb support to web-mode

--- init.el
+++ init.el
@@ -171,6 +171,7 @@
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
 ;;}}}
 
